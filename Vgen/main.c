bb6a60ed13f6b550a5236ad3cc7fab6a1c314a51
edwn112 <mrthapa322@gmail.com>
2018-12-23 03:27:32 +0530

    Initial Commit

---
+++ Vgen/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Vgen
+//
+//  Created by Amar Thapa on 23/12/18.
+//  Copyright © 2018 Amar Thapa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
